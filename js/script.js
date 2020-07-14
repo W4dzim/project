bd221730046f42fff5405baf6a42af3c7e2c110c
Vadim <example@e-mail.ru>
2020-07-14 17:25:03 +0300

    2nd part

--- js/script.js
+++ js/script.js
@@ -11,8 +11,48 @@ const personalMovieDB = {
   privat:false,
 };
 
+// for (let i = 0; i < 2; i++) {
+//   let ansF;
+//   let ansS;
+//   while (true) {
+//     ansF = prompt('last watched film?');
+//     if(ansF == '' || ansF == null || ansF.length > 50){      
+//       continue;
+//     } else {
+//       break;
+//     }
+//   }
+//   while (true) {
+//     ansS = prompt('your rate?');
+//     if (isNaN(ansS)) {
+//       continue;
+//     } else {
+//       break;
+//     }
+//   }
+//   personalMovieDB.movies[ansF] = ansS;
+// }
+
 for (let i = 0; i < 2; i++) {
-  // let queFirst = prompt('last watched film?');
-  // let queSec = prompt('your rate?');
-  personalMovieDB.movies[prompt('last watched film?')] = prompt('your rate?');
+  const a = prompt('last movie?'),
+        b = prompt('your rate?');
+  if (a != null && b != null && a != '' && a.length < 30 && !isNaN(b)) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    i--;
+    console.log('error');
+  }
+
+}
+
+
+
+
+if (personalMovieDB.count < 10) {
+  alert('pretty few movies watched');
+} else if (personalMovieDB.count > 10 && personalMovieDB < 30) {
+  alert('you are classical spectator');
+} else {
+  alert('fuck off');
 }
--- js/script2.js
+++ js/script2.js
@@ -10,3 +10,27 @@ const personalMovieDB = {
   genres:[],
   privat:false,
 };
+
+
+const num = 50;
+(num === 50) ? console.log('ok') : console.log('error');
+
+switch (num) {
+  case 49:
+    console.log('wrong');
+    break;
+  case 100:
+    console.log('wrong');
+    break;
+  case 50:
+    console.log('right');
+    break;
+  default:
+    console.log('next time');
+    break;
+}
+
+let num = 50;
+while (num < 54) {
+  console.log(++num);
+}
